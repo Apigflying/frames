71bb9ec17faaef5cad116bf98a50b72d46ee0c04
Apigflying <809276596@qq.com>
2018-05-30 22:35:17 +0800

    添加组件

---
+++ vues/Consoles/src/components/select/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-select v-model="value" placeholder="请选择" @change="change">
+    <el-option
+      v-for="item in options"
+      :key="item.value"
+      :label="item.label"
+      :value="item.value"
+      :disabled="item.disabled">
+    </el-option>
+  </el-select>
+</template>
+<script>
+export default {
+  props: {
+    options: {
+      type: Array,
+      required: true,
+      default: []
+    }
+  },
+  data () {
+    return {
+      value: ''
+    }
+  },
+  methods: {
+    change (value) {
+      console.log(value);
+    }
+  }
+}
+</script>
