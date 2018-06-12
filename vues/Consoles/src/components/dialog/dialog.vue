a01affe03a3142ea62f1e7326b7f40ff22e68b2d
Apigflying <809276596@qq.com>
2018-06-12 23:10:50 +0800

    dialog组件的丰富和优化

--- vues/Consoles/src/components/dialog/dialog.vue
+++ vues/Consoles/src/components/dialog/dialog.vue
@@ -1,8 +1,11 @@
 <template>
+<div>
+  <slot name="open"></slot>
   <el-dialog :title="title" :visible.sync="isDialogShow" width="50%" :before-close="closeDialog">
     <slot></slot>
     <slot name="custom"></slot>
   </el-dialog>
+</div>
 </template>
 
 <script>
