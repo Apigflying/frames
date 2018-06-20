5e0721057c410889951e1141cb57f10141e2bc37
Apigflying <809276596@qq.com>
2018-06-20 16:26:58 +0800

    拾起react

--- react/my_project/src/index.js
+++ react/my_project/src/index.js
@@ -1,6 +1,8 @@
 //这里是入口文件
 import React from 'react';
+// 将react生成的dom tree渲染到root根节点
 import ReactDOM from 'react-dom';
+// 引入根组件
 import App from './App';
 import axios from 'axios';
 //从react-router-dom引入react路由
@@ -12,5 +14,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 //将App挂在到根节点
 ReactDOM.render(
-  <Router><App/></Router>, document.getElementById('root')
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
 );
