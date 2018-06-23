77b10ca9413b103af5fa8f45a91ee770f3e49160
Apigflying <809276596@qq.com>
2018-06-23 21:28:34 +0800

    提交示例

--- react/todomvc/src/App.jsx
+++ react/todomvc/src/App.jsx
@@ -2,52 +2,22 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import 'view/public/public.scss';
 import {getData} from 'api/getDatas';
-
+import AddTodo from './AddTodo'
+import VisibleTodoList from './VisibleTodoList'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   async add () {
     let {data:{data}} = await getData();
     console.log(data);
   }
-  constructor(props) {
-    super(props);
-    this.incrementAsync = this.incrementAsync.bind(this);
-    this.incrementIfOdd = this.incrementIfOdd.bind(this);
-  }
-
-  incrementIfOdd() {
-    if (this.props.value % 2 !== 0) {
-      this.props.onIncrement()
-    }
-  }
-
-  incrementAsync() {
-    setTimeout(this.props.onIncrement, 1000)
-  }
-
   render() {
-    const { value, onIncrement, onDecrement } = this.props;
-    console.log(value)
     return (
-      <p>
-        Clicked: {value} times
-        {' '}
-        <button onClick={onIncrement}>
-          +
-        </button>
-        {' '}
-        <button onClick={onDecrement}>
-          -
-        </button>
-        {' '}
-        <button onClick={this.incrementIfOdd}>
-          Increment if odd
-        </button>
-        {' '}
-        <button onClick={this.incrementAsync}>
-          Increment async
-        </button>
-      </p>
+      <div>
+      <AddTodo />
+    <VisibleTodoList /></div>
     )
   }
 }
--- react/todomvc/src/index.js
+++ react/todomvc/src/index.js
@@ -2,24 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'style/reset/reset.scss';
 import 'style/index.scss';
-import { BrowserRouter as Router } from 'react-router-dom';
 import moment from 'moment';
+import {Provider} from 'react-redux';
 import store from 'store';
 import App from './App';
 
 const rootEle = document.getElementById('root');
 React.Component.prototype.$moment = moment;
 React.Component.prototype.$nextTick = process.nextTick;
-
-
-const render = ()=>ReactDOM.render(
-    <App
-      value={store.getState()}
-      onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
-      onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
-    />
-  ,
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
   rootEle
-);
-render();
-store.subscribe(render)
+)
