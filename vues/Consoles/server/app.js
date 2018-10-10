bd9fab9bb977706c980c8e12fee1a810d84444ec
Apigflying <809276596@qq.com>
2018-10-10 16:39:32 +0800

    vue instance server add

--- vues/Consoles/server/app.js
+++ vues/Consoles/server/app.js
@@ -2,8 +2,11 @@ import express from "express";
 import db from './mongoDB/db.js';
 import router from 'router/index.js';
 import configLite from 'config-lite';
+import bodyParser from 'body-parser';
+import history from 'connect-history-api-fallback';
 const config = configLite(__dirname);
 const app = express();
+
 //允许跨域请求
 if (config.alloworigin) {
   app.all('*', (req, res, next) => {
@@ -19,7 +22,16 @@ if (config.alloworigin) {
     }
   });
 }
+// 处理post请求的参数
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 router(app);
 
-app.listen(config.port);
+app.use(history());
+// 静态资源文件目录
+app.use(express.static(path.join(__dirname, '..', '/dist')));
+
+app.listen(config.port, () => {
+  console.log('server listen at http://localhost:' + config.port);
+});
